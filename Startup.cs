2cc9f32eaac6142d8d56cea793b5cee746878b11
fdz23 <mac01@Air-de-Mac01.nox5.lan>
2020-03-12 16:14:11 -0300

    minha api funciona e a sua nao

--- Startup.cs
+++ Startup.cs
@@ -18,6 +18,9 @@ namespace AppUser
 {
     public class Startup
     {
+
+        readonly string pacoca = "_pacoca";
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -28,6 +31,16 @@ namespace AppUser
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
+            services.AddCors(options =>
+        {
+            options.AddPolicy(pacoca,
+            builder =>
+            {
+                builder.WithOrigins("http://localhost:4200");
+            });
+        });
+
             services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
             services.AddDbContext<DataBaseContext>(options => 
                 options.UseSqlite("Data Source = Users.db"));
@@ -37,6 +50,8 @@ namespace AppUser
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseCors(pacoca);
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
